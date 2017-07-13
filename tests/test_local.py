2369faefad53ab46df87494d89abe81e4c2bd1e9
festinuz <festinuz@gmail.com>
2017-07-13 19:56:59 +0300

    Update tests

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,6 +1,8 @@
 import time
 import asyncio
 
+import pytest
+
 import gecaso
 
 
@@ -22,55 +24,30 @@ local_storage = gecaso.LocalMemoryStorage()
 local_async_storage = LocalAsyncMemoryStorage()
 
 
-@gecaso.cached(local_storage, ttl=5)
 def long_function(time_to_sleep):
     time.sleep(time_to_sleep)
     return time_to_sleep
 
 
-@gecaso.cached(local_storage, ttl=5)
 async def long_async_function(time_to_sleep):
     await asyncio.sleep(time_to_sleep)
     return time_to_sleep
 
 
-@gecaso.cached(local_async_storage, ttl=5)
-def another_long_function(time_to_sleep):
-    time.sleep(time_to_sleep)
-    return time_to_sleep
-
-
-@gecaso.cached(local_async_storage, ttl=5)
-async def another_long_async_function(time_to_sleep):
-    await asyncio.sleep(time_to_sleep)
-    return time_to_sleep
-
-
-def test_ss_wrapping():
+@pytest.mark.parametrize("storage", [local_storage, local_async_storage])
+def test_local_sync_function_cache(storage):
+    function = gecaso.cached(storage, ttl=5)(long_function)
     start_time = time.time()
     for i in range(10):
-        assert long_function(2) == 2
+        assert function(2) == 2
     assert time.time() - start_time < 5
 
 
-def test_as_wrapping():
+@pytest.mark.parametrize("storage", [local_storage, local_async_storage])
+def test_local_async_function_cache(storage):
+    function = gecaso.cached(storage, ttl=5)(long_async_function)
     loop = asyncio.get_event_loop()
     start_time = time.time()
     for i in range(10):
-        assert loop.run_until_complete(long_async_function(2)) == 2
-    assert time.time() - start_time < 5
-
-
-def test_sa_wrapping():
-    start_time = time.time()
-    for i in range(10):
-        assert another_long_function(2) == 2
-    assert time.time() - start_time < 5
-
-
-def test_aa_wrapping():
-    loop = asyncio.get_event_loop()
-    start_time = time.time()
-    for i in range(10):
-        assert loop.run_until_complete(another_long_async_function(2)) == 2
+        assert loop.run_until_complete(function(2)) == 2
     assert time.time() - start_time < 5
