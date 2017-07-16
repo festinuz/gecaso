2b24f283c3a514fc6f2b80090d31b8374f1e2b05
festinuz <festinuz@gmail.com>
2017-07-16 12:56:08 +0300

    Update cache function

--- gecaso/cache.py
+++ gecaso/cache.py
@@ -5,54 +5,59 @@ from . import utils
 from . import storage
 
 
-def cached(cache_storage, loop=None, **verfuns):
-    func = utils.Namespace()
+def cached(cache_storage, loop=None, **params):
     loop = loop or asyncio.get_event_loop()
     if isinstance(cache_storage, storage.BaseStorage):
-        async_storage = False
+        is_async_storage = False
     elif isinstance(cache_storage, storage.BaseAsyncStorage):
-        async_storage = True
+        is_async_storage = True
     else:
         raise ValueError('Provided storage is not subclass of base storage')
+    return _cached(cache_storage, is_async_storage, loop, **params)
+
+
+def _cached(cache_storage, is_async_storage, loop, **params):
+    function = None
 
     def ss_function(*args, **kwargs):
-        key = utils.make_key(func.self, *args, **kwargs)
+        key = utils.make_key(function, *args, **kwargs)
         try:
             result = cache_storage.get(key)
         except KeyError:
-            result = func.self(*args, **kwargs)
-            cache_storage.set(key, result, **verfuns)
+            result = function(*args, **kwargs)
+            cache_storage.set(key, result, **params)
         return result
 
     def sa_function(*args, **kwargs):
-        key = utils.make_key(func.self, *args, **kwargs)
+        key = utils.make_key(function, *args, **kwargs)
         try:
             result = loop.run_until_complete(cache_storage.get(key))
         except KeyError:
-            result = func.self(*args, **kwargs)
-            loop.run_until_complete(cache_storage.set(key, result, **verfuns))
+            result = function(*args, **kwargs)
+            loop.run_until_complete(cache_storage.set(key, result, **params))
         return result
 
     async def as_function(*args, **kwargs):
-        key = utils.make_key(func.self, *args, **kwargs)
+        key = utils.make_key(function, *args, **kwargs)
         try:
             result = cache_storage.get(key)
         except KeyError:
-            result = await func.self(*args, **kwargs)
-            cache_storage.set(key, result, **verfuns)
+            result = await function(*args, **kwargs)
+            cache_storage.set(key, result, **params)
         return result
 
     async def aa_function(*args, **kwargs):
-        key = utils.make_key(func.self, *args, **kwargs)
+        key = utils.make_key(function, *args, **kwargs)
         try:
             result = await cache_storage.get(key)
         except KeyError:
-            result = await func.self(*args, **kwargs)
-            await cache_storage.set(key, result, **verfuns)
+            result = await function(*args, **kwargs)
+            await cache_storage.set(key, result, **params)
         return result
 
-    def wrapper(function):
-        func.self = function
+    def wrapper(func):
+        nonlocal function
+        function = func
         async_function = inspect.iscoroutinefunction(function)
         wrappers = {
             (False, False): ss_function,  # first letter tells if function
@@ -60,7 +65,7 @@ def cached(cache_storage, loop=None, **verfuns):
             (True, False):  as_function,  # tells if cache storage is
             (True, True):   aa_function,  # asynchronous.
         }
-        wrapped_function = wrappers[(async_function, async_storage)]
+        wrapped_function = wrappers[(async_function, is_async_storage)]
         return utils.wrap(function, wrapped_function)
 
     return wrapper
