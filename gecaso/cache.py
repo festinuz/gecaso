5951e674adfed8579ada8e2cf52e2a0cf454c6f3
festinuz <festinuz@gmail.com>
2017-07-16 16:58:32 +0300

    Simplify _cached function

--- gecaso/cache.py
+++ gecaso/cache.py
@@ -1,5 +1,4 @@
 import asyncio
-import inspect
 
 from . import utils
 from . import storage
@@ -13,29 +12,26 @@ def cached(cache_storage, loop=None, **params):
 
 
 def _cached(cache_storage, loop, **params):
-    function = None
     storage_get = utils.asyncify(cache_storage.get)
     storage_set = utils.asyncify(cache_storage.set)
 
-    async def wrapped_function(*args, **kwargs):
-        key = utils.make_key(function, *args, **kwargs)
-        try:
-            result = await storage_get(key)
-        except KeyError:
-            result = await function(*args, **kwargs)
-            await storage_set(key, result)
-        return result
+    def wrapper(function):
+        async def wrapped_function(*args, **kwargs):
+            key = utils.make_key(function, *args, **kwargs)
+            try:
+                result = await storage_get(key)
+            except KeyError:
+                result = await function(*args, **kwargs)
+                await storage_set(key, result)
+            return result
 
-    def sync_wrapped_function(*args, **kwargs):
-        return loop.run_until_complete(wrapped_function(*args, **kwargs))
+        def sync_wrapped_function(*args, **kwargs):
+            return loop.run_until_complete(wrapped_function(*args, **kwargs))
 
-    def wrapper(func):
-        nonlocal function
-        function = func
-        if utils.is_async_function(func):
+        if utils.is_async_function(function):
             return utils.wrap(function, wrapped_function)
         else:
-            function = utils.asyncify(func)
+            function = utils.asyncify(function)
             return utils.wrap(function, sync_wrapped_function)
 
     return wrapper
