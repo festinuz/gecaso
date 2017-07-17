e3da9db7fc11df6efdc7bf91bb07213832ebaef0
festinuz <festinuz@gmail.com>
2017-07-17 19:27:11 +0300

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gecaso',
+  packages = ['gecaso'],
+  version = '0.1.1',
+  description = 'Generalized Caching Solution',
+  author = 'Michael Toporkov',
+  author_email = 'festinuz@gmail.com',
+  url = 'https://github.com/festinuz/gecaso',
+  download_url = f'{url}/archive/{version}.tar.gz',
+  keywords = ['cache', 'caching'],
+  classifiers = [],
+)
