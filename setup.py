112488958d288c6ed01d4647c02c550ef6798c87
festinuz <festinuz@gmail.com>
2017-07-18 19:33:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+
+version = '0.1.1'
+url = 'https://github.com/festinuz/gecaso'
+download_url = url + '/archive/' + version + '.tar.gz'
+
 setup(
-  name = 'gecaso',
-  packages = ['gecaso'],
-  version = '0.1.1',
-  description = 'Generalized Caching Solution',
-  author = 'Michael Toporkov',
-  author_email = 'festinuz@gmail.com',
-  url = 'https://github.com/festinuz/gecaso',
-  download_url = 'https://github.com/festinuz/gecaso/archive/0.1.1.tar.gz',
-  keywords = ['cache', 'caching'],
+  name='gecaso',
+  packages=['gecaso'],
+  version=version,
+  description='Generalized Caching Solution',
+  author='Michael Toporkov',
+  author_email='festinuz@gmail.com',
+  url=url,
+  download_url=download_url,
+  keywords=['cache', 'caching'],
   license='MIT',
-  classifiers = [],
+  classifiers=[],
   python_requires='>=3.5, <4'
 )
