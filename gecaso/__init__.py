ccdbf4841b3ceb63bb8902f192f8017868fa07c8
festinuz <festinuz@gmail.com>
2017-07-19 15:48:02 +0300

    Make "asyncify" helper function importable

--- gecaso/__init__.py
+++ gecaso/__init__.py
@@ -1,5 +1,7 @@
 from gecaso.cache import cached
 from gecaso.storage import BaseStorage, LocalMemoryStorage, LRUStorage
+from gecaso.utils import asyncify
 
 
-__all__ = ['cached', 'BaseStorage', 'LocalMemoryStorage', 'LRUStorage']
+__all__ = [
+    'cached', 'BaseStorage', 'LocalMemoryStorage', 'LRUStorage', 'asyncify']
